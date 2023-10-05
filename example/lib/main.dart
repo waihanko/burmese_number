9a4a9dc8cb48a43bf926baa3e32d8eeedc5d22a8
wai_han_ko <waihanko@tamronyangon.com>
2023-10-05 16:08:48 +0630

    - Initial Commit

---
+++ example/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:burmese_number/burmese_number.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Burmese Number'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController numberController = TextEditingController();
+  String longBurmesePlainText = "";
+  String longBurmeseWithNumber = "";
+  String simpleNumber = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(controller: numberController, decoration: const InputDecoration(
+              hintText: "Enter a number"
+            ),),
+            const SizedBox(height: 28,),
+            Text(simpleNumber),
+            const SizedBox(height: 28,),
+            Text(longBurmeseWithNumber),
+            const SizedBox(height: 26,),
+            Text(longBurmesePlainText),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+         longBurmesePlainText = BurmeseNumHelper.convertToLongNumber(number: numberController.text);
+         longBurmeseWithNumber = BurmeseNumHelper.convertToLongNumber(number:  numberController.text, numberFormat: NumberFormat.numberInText);
+         simpleNumber = BurmeseNumHelper.convertToSimpleNumber(number:  numberController.text);
+         setState(() {
+
+         });
+        },
+        child: const Icon(Icons.play_arrow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/burmese_number.dart
@@ -0,0 +1,234 @@
+library burmese_number;
+
+class BurmeseNumHelper {
+
+
+  static final List<String> _burmeseNumberInText = [
+    "",
+    "ဆယ်",
+    "ရာ",
+    "ထောင်",
+    "သောင်း",
+    "သိန်း",
+    "သန်း",
+    "ကုဋေ",
+  ];
+
+
+  static final Map<String, String> _numberToBurmeseNumberMapper = {
+    "0": "၀",
+    "1": "၁",
+    "2": "၂",
+    "3": "၃",
+    "4": "၄",
+    "5": "၅",
+    "6": "၆",
+    "7": "၇",
+    "8": "၈",
+    "9": "၉"
+  };
+
+   static String convertToSimpleNumber({
+    required String number,
+    bool isCommaSeparated = false,
+  }) {
+    if (isCommaSeparated) {
+      RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+      String mathFunc(Match match) => '${match[1]},';
+      number = number.replaceAllMapped(reg, mathFunc);
+    }
+
+    return _numberToBurmeseNumberMapper.entries
+        .fold(number, (prev, e) => prev.toString().replaceAll(e.key, e.value));
+  }
+
+
+  //Over 8 && NORMAL => Change To THEIN
+  //Over 10 && THEIN => Change To THAN
+  //Over 12 && THAN => Change To GADAY
+  //Over 14 is Invalid
+
+  static String convertToLongNumber({
+    required String number,
+    NumberFormat numberFormat = NumberFormat.plainText,
+  }) {
+    String result = "";
+    if(!RegExp(r'^(?![^0-9.]+$)(\d+(\.\d+)?|\.\d+)$').hasMatch(number)) return "INVALID";
+    DisplayFormat displayFormat  = DisplayFormat.defaultType;
+
+    List<String> originalNumber = number.split(".");
+    if (originalNumber[0].length > 14) {
+      return "INVALID";
+    } else if (originalNumber[0].length > 12 ) {
+      displayFormat = DisplayFormat.d8;
+    } else if (originalNumber[0].length > 10) {
+      displayFormat = DisplayFormat.d7;
+    } else if (originalNumber[0].length > 6) {
+      displayFormat = DisplayFormat.d6;
+    }
+
+    result += _getFullNumber(
+        originalNumber[0].substring(
+            0, originalNumber[0].length - displayFormat.getPartition.index),
+        numberFormat,
+        originalNumber[0].length);
+
+    if (originalNumber[0]
+        .substring(0, originalNumber[0].length - displayFormat.getPartition.index)
+        .endsWith("0")) {
+      result = displayFormat.getPartition.name + result;
+    } else {
+      result = result + displayFormat.getPartition.name;
+    }
+
+    result += _getFullNumber(
+        originalNumber[0].substring(
+            originalNumber[0].length - displayFormat.getPartition.index,
+            originalNumber[0].length),
+        numberFormat,
+        originalNumber[0].length);
+
+    if (originalNumber.length > 1) {
+      result += ".".getMMDescription;
+
+      originalNumber[1].split("").asMap().forEach((index, element) {
+        result += numberFormat == NumberFormat.plainText
+            ? element.trim().getMMDescription
+            : element.trim().getMMNumber;
+      });
+    }
+
+    return result;
+  }
+
+  static String _getFullNumber(
+      String wholeNumber,
+      NumberFormat mmNumberType,
+      int digitCount,
+      ) {
+    String result = "";
+    List<String> numType2 = List.of(_burmeseNumberInText);
+
+    if (digitCount >= 2 && !wholeNumber.endsWith("0")) {
+      numType2[1] = "ဆယ့်";
+    }
+    if (digitCount >= 3 && !wholeNumber.endsWith("00")) {
+      numType2[2] = "ရာ့";
+    }
+    if (digitCount >= 4 && !wholeNumber.endsWith("000")) {
+      numType2[3] = "ထောင့်";
+    }
+
+    wholeNumber.split("").reversed.toList().asMap().forEach((index, element) {
+      if (element == "0") return;
+      String prefix = mmNumberType == NumberFormat.plainText
+          ? element.trim().getMMDescription
+          : element.trim().getMMNumber;
+      String postFix = numType2[index];
+      result = prefix + postFix + result;
+    });
+    return result;
+  }
+}
+
+extension ConvertToSimpleNumberExtension on String {
+  String convertToSimpleNumber({bool isCommaSeparated = false}) {
+    return BurmeseNumHelper.convertToSimpleNumber(number: this, isCommaSeparated: isCommaSeparated);
+  }
+
+  String convertToLongNumber({NumberFormat numberType = NumberFormat.plainText,}) {
+    return BurmeseNumHelper.convertToLongNumber(number: this, numberFormat: numberType);
+  }
+
+}
+
+extension ExtendedString on String {
+  String get getMMDescription {
+    switch (this) {
+      case ".":
+        return "ဒသမ";
+      case "0":
+        return "သုည";
+      case "1":
+        return "တစ်";
+      case "2":
+        return "နှစ်";
+      case "3":
+        return "သုံး";
+      case "4":
+        return "လေး";
+      case "5":
+        return "ငါး";
+      case "6":
+        return "ခြောက်";
+      case "7":
+        return "ခုနှစ်";
+      case "8":
+        return "ရှစ်";
+      case "9":
+        return "ကိုး";
+      default:
+        return "";
+    }
+  }
+
+  String get getMMNumber {
+    switch (this) {
+      case ".":
+        return ".";
+      case "0":
+        return "၀";
+      case "1":
+        return "၁";
+      case "2":
+        return "၂";
+      case "3":
+        return "၃";
+      case "4":
+        return "၄";
+      case "5":
+        return "၅";
+      case "6":
+        return "၆";
+      case "7":
+        return "၇";
+      case "8":
+        return "၈";
+      case "9":
+        return "၉";
+      default:
+        return "";
+    }
+  }
+}
+
+extension DisplayFormatExt on DisplayFormat {
+  DisplayFormatVo get getPartition {
+    switch (this) {
+      case DisplayFormat.d6:
+        return DisplayFormatVo(5, "သိန်း");
+      case DisplayFormat.d7:
+        return DisplayFormatVo(6, "သန်း");
+      case DisplayFormat.d8:
+        return DisplayFormatVo(7, "ကုဋေ");
+      case DisplayFormat.defaultType:
+        return DisplayFormatVo(0, "");
+      default:
+        return DisplayFormatVo(0, "");
+    }
+  }
+}
+
+class DisplayFormatVo {
+  final int index;
+  final String name;
+
+  DisplayFormatVo(this.index, this.name);
+}
+
+enum NumberFormat { plainText, numberInText }
+
+//d6 - thein,
+//d7 - than
+//d8 - gaday
+enum DisplayFormat { d6, d7, d8, defaultType }
